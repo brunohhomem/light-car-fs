28abba9d91aa51ac2ac507e8331f2f60368e982d
Bruno H. Homem <brunohhomem@gmail.com>
2024-11-26 06:58:22 -0300

    chore: fix dotenv maps

--- frontend/src/components/ride-map.tsx
+++ frontend/src/components/ride-map.tsx
@@ -7,10 +7,13 @@ interface MapProps {
 }
 
 export default function Map({ estimateData }: MapProps) {
-  const { origin, destination } = estimateData
-  const googleMapsApiKey = process.env.GOOGLE_API_KEY
+  const { origin, destination, routeResponse } = estimateData
+  const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY
 
   const mapUrl = `https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=roadmap&markers=color:blue%7Clabel:O%7C${origin.latitude},${origin.longitude}&markers=color:red%7Clabel:D%7C${destination.latitude},${destination.longitude}&path=color:0xff0000ff|weight:2|${origin.latitude},${origin.longitude}|${destination.latitude},${destination.longitude}&key=${googleMapsApiKey}`
+
+  console.log(routeResponse)
+  // console.log(routeResponse.routes[0].legs)
 
   return (
     <div className="w-full max-w-md">
